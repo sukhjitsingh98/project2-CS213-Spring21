bbe7dcb28d15f69bb66d1d62e70c29b73cd2f003
Sukhjit Singh <ssukhjit1998@gmail.com>
2021-02-09 01:11:47 -0500

    Added code for Profile and RunProject2 classes

--- src/Profile.java
+++ src/Profile.java
@@ -1,2 +1,13 @@
 public class Profile {
+    private String name; //employee’s name in the form “lastname,firstname”
+    private String department; //department code: CS, ECE, IT
+    private Date dateHired;
+    @Override
+    public String toString() {
+        return "";
+    }
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    } //compare name, department and dateHired
 }
--- src/RunProject2.java
+++ src/RunProject2.java
@@ -1,2 +1,14 @@
+/**
+ Contains the main method which calls the PayrollProcessing class in order to initiate the program.
+ @author German Munguia, Sukhjit Singh
+ */
 public class RunProject2 {
+    /**
+     Calls the run method in the Kiosk class.
+     @param args Unused parameter
+     */
+    public static void main(String[] args) {
+        //Uncomment when ready
+        //new PayrollProcessing().run();
+    }
 }
