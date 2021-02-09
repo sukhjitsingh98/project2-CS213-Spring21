55988b313bb75a1c187f06f751a99c600bb8ca9f
Sukhjit Singh <ssukhjit1998@gmail.com>
2021-02-09 01:07:11 -0500

    Added code for Company class

--- src/Company.java
+++ src/Company.java
@@ -1,2 +1,22 @@
 public class Company {
+    private Employee[] emplist;
+    private int numEmployee;
+
+    private int find(Employee employee) {
+        return -1;
+    }
+    private void grow() { }
+    public boolean add(Employee employee) {
+        return false;
+    } //check the profile before adding
+    public boolean remove(Employee employee) {
+        return false;
+    } //maintain the original sequence
+    public boolean setHours(Employee employee) {
+        return false;
+    } //set working hours for a part time
+    public void processPayments() { } //process payments for all employees
+    public void print() { } //print earning statements for all employees
+    public void printByDepartment() { } //print earning statements by department
+    public void printByDate() { } //print earning statements by date hired
 }
