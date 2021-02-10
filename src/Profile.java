bdfa683a1b99d5ce4ba13da22ec71c2e67afe059
GermanMunguia <munguiagerman48@gmail.com>
2021-02-10 17:01:37 -0500

    Added getters

--- src/Profile.java
+++ src/Profile.java
@@ -4,10 +4,28 @@ public class Profile {
     private Date dateHired;
     @Override
     public String toString() {
-        return "";
+        String result = name + "::" + department + "::" + dateHired.getMonth() + "/" + dateHired.getDay() + "/" + dateHired.getYear() + "::"; //once the earnings portion is added this will be finished
+
+        return result;
     }
     @Override
     public boolean equals(Object obj) {
         return false;
     } //compare name, department and dateHired
+
+    //returns the name of the employees profile
+    public String getName() {
+        return name;
+    }
+
+    //return the department
+    public String getDepartment() {
+        return department;
+    }
+
+    //returns dateHired
+    public Date getDateHired() {
+        return dateHired;
+    }
 }
+
